3f2c728e4375b39941e4d4a0bddb73076628dfd6
Henrique Beserra <millerhiquinho@gmail.com>
2023-12-02 13:33:26 -0300

    Desenvolvendo as funções de controlers que lidam com as inforrmações do usuário

---
+++ frontend/javascript/controlers.js
@@ -0,0 +1,38 @@
+
+function organizaIformacoes (value1, value2, value3) {
+
+    let bd  = [];
+    let doc = {};
+    doc.mat = value1
+    doc.pw  = value2
+
+    bd.push(doc);
+
+    return bd;
+
+} 
+
+async function coletaInformacoes () {
+
+    const formData = new FormData(document.querySelector('form'));
+    const inputMatricula_value = await formData.get('numero-de-matricula') ;
+    const inputData = await formData.get('pw');
+
+    const processData = organizaIformacoes(inputMatricula_value, inputData)
+    limpaForm();
+
+    return processData;
+
+}; 
+
+function limpaForm () {
+    const matricula = document.querySelector("#numero-de-matricula");
+    const senha     = document.querySelector('#senha');
+    
+    matricula.value = '';
+    senha.value     = '';
+}
+
+
+
+
